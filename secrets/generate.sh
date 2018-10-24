2e817a6658088c36d1b45c72efbda21fb7996bfa
Junhong Liang <junhong.liang@fit.fraunhofer.de>
2018-10-24 03:53:37 -0400

    Removed interactive script

--- secrets/generate.sh
+++ secrets/generate.sh
@@ -1,56 +1,56 @@
 #!/bin/ash -e
 
-KEYSTORE_PSWD=$1
-CLIENT_DNAME=$2
-TRUSTSTORE_PSWD=$3
+SERVER_DNAME=$1
+KEYSTORE_PSWD=$2
+CLIENT_DNAME=$3
+TRUSTSTORE_PSWD=$4
+GENERATE_EXT_TRUSTSTORE=$5
+EXT_TRUSTSTORE_PSWD=$6
+CLIENT_PSWD=$7
+
+echo "--------- Processing keystore/truststore ---------"
 
 if [ ! -f ./secrets/keystore.jks ]; then
     echo "keystore.jks does not exist. Generating new keystore."
-    read -p "Please enter the subject of cert. It typically has the form \"CN=[hostname],OU=nifi\":" SERVER_CERT_SUBJECT
-    echo "---------------------------------------------"
-    echo "Generating keystore with certificate: ${SERVER_CERT_SUBJECT}"
-    echo "---------------------------------------------"
-    keytool -genkey -keyalg RSA -alias nifi -keystore keystore.jks -keypass "${KEYSTORE_PSWD}" -storepass "${KEYSTORE_PSWD}" -validity 365 -keysize 4096 -dname "${SERVER_CERT_SUBJECT}"
+    echo "*** Generating keystore with certificate \"${SERVER_DNAME}\" ***"
+    keytool -genkey -keyalg RSA -alias nifi -keystore keystore.jks -keypass "${KEYSTORE_PSWD}" -storepass "${KEYSTORE_PSWD}" -validity 365 -keysize 4096 -dname "${SERVER_DNAME}"
+    chmod 640 keystore.jks
 
     # Generate a truststore for this keystore, so that it could be easily used on other Nifi instances to communicate with this Nifi instance securely
-    rm -f external.der
-    rm -f external_truststore.jks
-    echo -n "Generating a truststore for EXTERNAL usage from this keystore. Please provide password for this truststore: "
-    read -s EXTERNAL_TRUSTSTORE_PSWD
-    echo " "
-    keytool -export -keystore keystore.jks -alias nifi -file external.der -storepass "${KEYSTORE_PSWD}"
-    keytool -import -file external.der -alias nifi -keystore external_truststore.jks -storepass "${EXTERNAL_TRUSTSTORE_PSWD}" -noprompt
+    if [ "${GENERATE_EXT_TRUSTSTORE}" == "YES" ]; then
+        echo "*** Generating EXTERNAL truststore ***"
+        rm -f external.der
+        rm -f external_truststore.jks
+        keytool -export -keystore keystore.jks -alias nifi -file external.der -storepass "${KEYSTORE_PSWD}"
+        keytool -import -file external.der -alias nifi -keystore external_truststore.jks -storepass "${EXT_TRUSTSTORE_PSWD}" -noprompt
+        echo "${EXT_TRUSTSTORE_PSWD}" > ./external_truststore.pass
+        rm -f external.der
+        chmod 640 external_truststore.jks
+    fi
 
-    # Clean up
-    rm -f external.der
-    chmod 640 keystore.jks
-    chmod 640 external_truststore.jks
-    echo "---------------------------------------------"
-    echo "Keystore generation finished!"
-    echo "---------------------------------------------"
+    echo "*** Keystore generation finished! ***"
 fi
 
 # Generate a JKS keystore first
 if [ ! -f ./truststore.jks ]; then
+
+
     # Generate truststore and client key
     echo "truststore.jks does not exist. Generating new truststore.jks"
-    echo "---------------------------------------------"
-    echo "Generating truststore with subject field: ${CLIENT_DNAME}"
-    echo "---------------------------------------------"
+    echo "*** Generating truststore with certificate \"${CLIENT_DNAME}\" ***"
     # First generate a keystore
     keytool -genkey -keyalg RSA -alias client -keystore client_keystore.jks -keypass "${TRUSTSTORE_PSWD}" -storepass "${TRUSTSTORE_PSWD}" -validity 365 -keysize 4096 -dname "${CLIENT_DNAME}"
 
     # Export certificate and put it into truststore
-    echo -n "Generating a PKCS12 file for client. Please provide password for this PCKS12 file: "
-    read -s PKCS12_PSWD
-    echo " "
     keytool -export -keystore client_keystore.jks -alias client -file client.der -storepass "${TRUSTSTORE_PSWD}"
     keytool -import -file client.der -alias client -keystore truststore.jks -storepass "${TRUSTSTORE_PSWD}" -noprompt
 
     # Generate pkcs12 file from client_keystore.jks
+    rm -f client.p12
     keytool -importkeystore -srckeystore client_keystore.jks -destkeystore client.p12 \
-      -srcstoretype JKS -deststoretype PKCS12 -srcstorepass "${TRUSTSTORE_PSWD}" -deststorepass "${PKCS12_PSWD}" -destkeypass "${PKCS12_PSWD}" \
+      -srcstoretype JKS -deststoretype PKCS12 -srcstorepass "${TRUSTSTORE_PSWD}" -deststorepass "${CLIENT_PSWD}" -destkeypass "${CLIENT_PSWD}" \
       -srcalias client -destalias client
+    echo "${CLIENT_PSWD}" > ./client.pass
 
     # Clean up
     rm -f client_keystore.jks
@@ -59,6 +59,7 @@ if [ ! -f ./truststore.jks ]; then
     chmod 640 truststore.jks
 fi
 
+echo "--------- Processing finished ---------"
 
 
 
--- setup.sh
+++ setup.sh
@@ -1,82 +1,247 @@
 #!/bin/bash -e
 
-echo " ----------------------------------------------"
-echo "|                                              |"
-echo "|  Secure Nifi with cert-based authentication  |"
-echo "|                                              |"
-echo " ----------------------------------------------"
+cat << EOF
+     ----------------------------------------------
+    |                                              |
+    |  Secure Nifi with cert-based authentication  |
+    |                                              |
+     ----------------------------------------------
+EOF
 
-# Function to generate random password
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+key="$1"
+    case $key in
+        -h|--help)
+        HELP=YES
+        shift # past argument
+        ;;
+        -n|--hostname)
+        NIFI_HOST="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -p|--port)
+        NIFI_PORT="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --keystore)
+        KEYSTORE="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --new-keystore)
+        NEW_KEYSTORE=YES
+        shift # past argument
+        ;;
+        --key-pass)
+        KEYSTORE_PASS="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --truststore)
+        TRUSTSTORE="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --new-truststore)
+        NEW_TRUSTSTORE=YES
+        shift # past argument
+        ;;
+        --trust-pass)
+        TRUSTSTORE_PASS="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --ext-trust)
+        GENERATE_EXT_TRUSTSTORE=YES
+        shift # past argument
+        ;;
+        --ext-pass)
+        EXT_TRUSTSTORE_PASS="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -c|--client-dn)
+        CLIENT_DN="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --client-pass)
+        CLIENT_PASS="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -s|--server-dn)
+        SERVER_DN="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+print_help(){
+
+    cat << EOF
+
+    This script generate appropriate configuration and keystore/truststore for a secure Nifi instance.
+
+    USAGE: ./setup.sh [OPTIONS] [ARGUMENTS]
+
+    Example: ./setup.sh -n host-01 -p 8443 --new-keystore --new-truststore -c "CN=admin, OU=nifi" -s "CN=host-01,OU=nifi"
+
+    OPTIONS:
+
+EOF
+    cat << EOF | column -s"|" -t
+    -h, --help:|Show the help message.
+    -n, --hostname HOSTNAME:|Required. The hostname of machine hosting the Nifi container.
+    -p, --port PORT:|Required. The forwarded port to the Nifi UI.
+    --keystore FILE:|Optional. The keystore file to be used in Nifi. If this argument is set, --keypass must also be set.
+    --new-keystore:|Optional. Create new keystore. Either this flag or --keystore must be specified.
+    --keypass PASSWORD:|Optional. The password to specified keystore or the newly generated one. Must be specified when --keystore is set and must match the password of the specified keystore file. If not specified, a random one will be used.
+    --truststore FILE:|Optional. The truststore file to be used in Nifi. If this argument is set, --trustpass must also be set.
+    --new-truststore:|Optional. Create new truststore. Either this flag or --truststore must be specified.
+    --trustpass PASSWORD:|Optional. The password to the specified truststore or the newly generated one. Must be specified when --truststore is set and must match the password of the specified keystore file. If not specified, a random one will be used.
+    --ext-trust:|Optional. Whether to generate a truststore from the keystore, which is intended to be used by another Nifi instance to communicate securely with this one.
+    --ext-pass PASSWORD:|Optional. The password to the external truststore. If not specified, a random one is used.
+    --client-pass PASSWORD:|Optional. The password to the client key file. If not specified, a random one is used.
+    -s, --server-dn DN:|Optional. The Distinguish Name of the server certificate in keystore (Default: CN=[HOSTNAME],OU=nifi).
+    -c, --client-dn DN:|Optional. The Distinguish Name of the client certificate in truststore. MUST use SPACES to separate domain components (Default: CN=user ,OU=nifi).
+EOF
+
+}
+
 gen_pass(){
     echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 }
 
-# If an old .env file exists, remove it
-if [ -f ./.env ]; then
-    rm -f ./.env
+# Check the correctness of arguments
+if [ ! -z "${HELP}" ]; then
+    print_help
+    exit 0
+fi
+if [ -z "${NIFI_HOST}" ]; then
+    echo "[ERROR] \"-n | --hostname\" is not specified "
+    print_help
+    exit 1
+fi
+if [ -z "${NIFI_PORT}" ]; then
+    echo "[ERROR] \"-p | --port\" is not specified "
+    print_help
+    exit 1
+fi
+if [ ! -z "${KEYSTORE}" -a -z "${KEYSTORE_PASS}" ]; then
+    echo "[ERROR] keystore specified but no keystore pass is given"
+    print_help
+    exit 1
+fi
+if [ ! -z "${TRUSTSTORE}" -a -z "${TRUSTSTORE_PASS}" ]; then
+    echo "[ERROR] truststore specified but no truststore pass is given"
+    print_help
+    exit 1
+fi
+if [ -z "${KEYSTORE}" -a -z "${NEW_KEYSTORE}" ]; then
+    echo "[ERROR] Either --keystore or --new-keystore must be specified"
+    exit 1
+fi
+if [ -z "${TRUSTSTORE}" -a -z "${NEW_TRUSTSTORE}" ]; then
+    echo "[ERROR] Either --truststore or --new-truststore must be specified"
+    exit 1
+fi
+if [ ! -z "${KEYSTORE}" -a ! -f "${KEYSTORE}" ]; then
+    echo "[ERROR] ${KEYSTORE} does not exist."
+    exit 1
+fi
+if [ ! -z "${TRUSTSTORE}" -a ! -f "${TRUSTSTORE}" ]; then
+    echo "[ERROR] ${TRUSTSTORE} does not exist."
+    exit 1
 fi
 
-read -p "The hostname of the machine running Nifi container:" NIFI_HOST
-read -p "The host's forwarded port to the Nifi UI: " NIFI_PORT
-
-if [ -f ./secrets/keystore.jks ]; then
-    echo -n "keystore.jks detected. Please provide the password for the the keystore: "
-    read -s NIFI_KEYSTORE_PASS
-    echo " "
-else
-    NIFI_KEYSTORE_PASS=$(gen_pass)
+# If both --keystore and --new-keystore is specified, ignore --new-keystore
+if [ ! -z "${KEYSTORE}" -o ! -z "${NEW_KEYSTORE}" ]; then
+    unset NEW_KEYSTORE
+fi
+if [ ! -z "${TRUSTSTORE}" -o ! -z "${NEW_TRUSTSTORE}" ]; then
+    unset NEW_TRUSTSTORE
 fi
 
-if [ -f ./secrets/truststore.jks ]; then
-    echo -n "truststore.jks detected. Please provide the password for the the truststore: "
-    read -s NIFI_TRUSTSTORE_PASS
-    echo " "
-else
-    NIFI_TRUSTSTORE_PASS=$(gen_pass)
+if [ ! -z "${NEW_KEYSTORE}" ]; then
+    rm -f ./secrets/keystore.jks
 fi
-echo "Please enter the DN of client. It typically has the form \"CN=user, OU=nifi\""
-read -p "IMPORTANT: the SPACE between components must be provided as the example above:" CLIENT_DNAME
+if [ ! -z "${NEW_TRUSTSTORE}" ]; then
+    rm -f ./secrets/truststore.jks
+fi
 
-# Run the script to generate keystore/truststore, if they don't exist
+# Prepare files
+if [ ! -z "${KEYSTORE}" ]; then
+    mv -f "${KEYSTORE}" ./secrets/keystore.jks
+fi
+if [ ! -z "${TRUSTSTORE}" ]; then
+    mv -f "${TRUSTSTORE}" ./secrets/truststore.jks
+fi
+
+# Give default values
+: ${CLIENT_DN:="CN=user, OU=nifi"}
+: ${SERVER_DN:="CN=${NIFI_HOST},OU=nifi"}
+: ${KEYSTORE_PASS:=$(gen_pass)}
+: ${TRUSTSTORE_PASS:=$(gen_pass)}
+: ${GENERATE_EXT_TRUSTSTORE:=NO}
+: ${EXT_TRUSTSTORE_PASS:=$(gen_pass)}
+: ${CLIENT_PASS:=$(gen_pass)}
+
+# Generate keystore/truststore
 docker run -it --rm -v "$PWD/secrets":/usr/src/secrets \
         -w /usr/src/secrets --user ${UID} openjdk:8-alpine \
         /usr/src/secrets/generate.sh \
-        "${NIFI_KEYSTORE_PASS}" "${CLIENT_DNAME}" "${NIFI_TRUSTSTORE_PASS}"
+        "${SERVER_DN}" "${KEYSTORE_PASS}" \
+        "${CLIENT_DN}" "${TRUSTSTORE_PASS}" \
+        "${GENERATE_EXT_TRUSTSTORE}" "${EXT_TRUSTSTORE_PASS}" \
+        "${CLIENT_PASS}"
+
 
 echo "Setting up .env file..."
 cat << EOF > ./.env
 AUTH=tls
-INITIAL_ADMIN_IDENTITY=${CLIENT_DNAME}
+INITIAL_ADMIN_IDENTITY=${CLIENT_DN}
 KEYSTORE_PATH=/opt/secrets/keystore.jks
 KEYSTORE_TYPE=JKS
-KEYSTORE_PASSWORD=${NIFI_KEYSTORE_PASS}
+KEYSTORE_PASSWORD=${KEYSTORE_PASS}
 TRUSTSTORE_PATH=/opt/secrets/truststore.jks
-TRUSTSTORE_PASSWORD=${NIFI_TRUSTSTORE_PASS}
+TRUSTSTORE_PASSWORD=${TRUSTSTORE_PASS}
 TRUSTSTORE_TYPE=JKS
 NIFI_WEB_PROXY_HOST=${NIFI_HOST}:${NIFI_PORT}
 NIFI_WEB_HTTP_HOST=${NIFI_HOST}
 NIFI_REMOTE_INPUT_HOST=${NIFI_HOST}
 EOF
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~"
-echo "Setup is done!"
-echo "~~~~~~~~~~~~~~~~~~~~~~~"
-echo "You can now run:"
-echo " "
-echo "   docker build -t secure-nifi ."
-echo " "
-echo "to build the image. Then use this command to run the container:"
-echo " "
-echo "   docker run --name secure-nifi --env-file ./.env -p ${NIFI_PORT}:8443 --detach secure-nifi"
-echo " "
-echo "To visit the Nifi UI, you need to import the client key into your browser. The key file is located in:"
-echo " "
-echo "   ./secrets/client.p12"
-echo " "
-echo "After importing, you can visit the following URL for the Nifi UI:"
-echo " "
-echo "   https://${NIFI_HOST}:${NIFI_PORT}/nifi"
-echo " "
-echo "Happy flowing!"
-echo " "
-echo " "
+cat << EOF
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            Setup is done!
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+You can now run:
+
+   docker build -t secure-nifi .
+
+to build the image. Then run the container:
+
+   docker run --name secure-nifi --env-file ./.env -p ${NIFI_PORT}:8443 --detach secure-nifi
+
+To visit the Nifi UI, you need to import the client key into your browser. The key file is located in:
+
+   ./secrets/client.p12
+
+After importing, you can visit the following URL for the Nifi UI:
+
+   https://${NIFI_HOST}:${NIFI_PORT}/nifi
+
+Happy flowing!
+
+
+EOF
